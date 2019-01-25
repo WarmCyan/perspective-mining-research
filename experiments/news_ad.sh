d1bd3dee95ab00643edb2dd07cb010d00e9c2e3e
Nathan Martindale <nathanamartindale@gmail.com>
2019-01-24 22:43:00 -0600

    Increased article count in experiment

--- experiments/news_ad.sh
+++ experiments/news_ad.sh
@@ -2,10 +2,12 @@
 
 pushd ../src
 
-sentencify.py -i ../data/raw/kaggle1 -o ../data/cache/kaggle1_sentences.dat -c 1000 --overwrite
+# 100,000,000
+#sentencify.py -i ../data/raw/kaggle1 -o ../data/cache/kaggle1_sentences.dat -c 10000000000 --overwrite
+sentencify.py -i ../data/raw/kaggle1 -o ../data/cache/kaggle1_sentences.dat --overwrite
 word2vec.py -i ../data/cache/kaggle1_sentences.dat -o ../data/cache/kaggle1_w2v_model --overwrite
 
-preprocess_kaggle.py -i ../data/raw/kaggle1 -o ../data/cache/kaggle1_preprocessed.dat -c 100 --overwrite
+preprocess_kaggle.py -i ../data/raw/kaggle1 -o ../data/cache/kaggle1_preprocessed.dat -c 10000 --overwrite
 
 pushd aspect_detection/neural_attn_model
 bash -c ". bin/activate && cd code && python train.py -o ../../../../data/cache/test -i ../../../../data/cache/kaggle1_preprocessed.dat --emb ../../../../data/cache/kaggle1_w2v_model"
