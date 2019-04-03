feca5f6c06109a4e78c4e9d2088ee1df460767a7
Nathan Martindale <nathanamartindale@gmail.com>
2019-04-02 20:58:30 -0500

    Change as_webhose run to use all documents

--- experiments/as_webhose.sh
+++ experiments/as_webhose.sh
@@ -6,7 +6,7 @@ prune_articles.py -i ../data/raw/webhose_political/news.csv -o ../data/cache/web
 docify.py -i ../data/cache/webhose -o ../data/cache/webhose.json --col text
 
 pushd aspect_detection/bootstrap
-detection.py -i ../../../data/cache/webhose.json -o ../../../data/cache/webhose_aspects -c 1000 --overwrite
+detection.py -i ../../../data/cache/webhose.json -o ../../../data/cache/webhose_aspects
 popd
 
 pushd sentiment_analysis
