009f12033d110459370e6558cf2e99c9660658b0
Nathan Martindale <nathanamartindale@gmail.com>
2019-07-24 21:27:23 -0500

    Finished preprocessing and vectorizing stages of new experiment pipeline

--- perspective/experiments.py
+++ perspective/experiments.py
@@ -3,8 +3,10 @@
 import pandas as pd
 import argparse
 import logging
-import os
 import json
+import shutil
+import os
+import datetime
 
 import hashlib
 
@@ -12,10 +14,20 @@ from tqdm import tqdm
 import utility
 
 import docify
+import tokenization
+import aspect_detection
+import sentiment_analysis
+import tfidfify
+import combine
 
-def run(experiment_path, raw_path, cache_path, overwite=False):
 
-    experiment_list = []
+THREAD_COUNT = 4
+
+
+# in this context overwrite means re-run existing experiments
+def run(experiment_path, raw_path, cache_path, overwrite=False):
+
+    experiment_list = [] # populate this from some other folder
 
     preprocess_hashes = {}
     vectorize_hashes = {}
@@ -23,43 +35,110 @@ def run(experiment_path, raw_path, cache_path, overwite=False):
 
     results_list = []
 
+    results_path = experiment_path + "/results.csv"
+
+    # search for previous results file, and load them in
+    if os.path.exists(results_path):
+        old_results_df = pd.read_csv(results_path)
+        results_list = old_results_df.to_dict(orient='records')
+
+    # iterate through each experiment
     index = 0
     for experiment in experiment_list:
         index += 1
+        logging.info("################################################")
         logging.info("Starting experiment %i - %s", index, json.dumps(experiment))
+        logging.info("################################################")
 
-        # NOTE: each step should return a set of parameters to record
-
-        # preprocessing
-        preprocess_params = experiment[0]
+        # get experiment parameter hashes
+        preprocess_params = experiment["preprocess"]
         preprocess_hash = hash_params(preprocess_params)
-        logging.info("Running preprocessing for experiment %i - %s - %s", index, preprocess_hash, json.dumps(preprocess_params))
-                
-        if preprocess_hash in preprocess_params.keys():
-            preprocess_hashes[preprocess_hash] = preprocess_params
-        preprocess(**preprocess_params)
-
-        # vectorizing
-        vectorize_params = experiment[1]
+        vectorize_params = experiment["vectorize"]
         vectorize_hash = hash_params(vectorize_params)
-        logging.info("Running vectorization for experiment %i - %s - %s", index, vectorize_hash, json.dumps(vectorize_params))
-        
-        if vectorize_hash in vectorize_params.keys():
-            vectorize_hashes[vectorize_hash] = vectorize_params
-        vectorize(**vectorize_params)
-
-        predict_params = experiment[1]
+        predict_params = experiment["predict"]
         predict_hash = hash_params(predict_params)
         
+        # determine output folders for each stage
+        preprocess_folder = experiment_path + "/preproc_" + preprocess_hash
+        vectorize_folder = experiment_path + "/vec_" + vectorize_hash
+        predict_folder = experiment_path + "/pred_" + predict_hash
+
+        # check if experiment already run
+        exists = False
+        if old_results_df[(old_results_df.preprocess == preprocess_hash) & (old_results_df.vectorize == vectorize_hash) & (old_results_df.predict == predict_hash)].shape[0] > 0:
+            exists = True
+        
+        if exists:
+            logging.info("Previous result from experiment found...")
+            if overwrite or ("rerun" in experiment.keys() and experiment["rerun"]):
+                logging.info("Skipping")
+                continue
+            else:
+                logging.info("Deleting previous experiment components...")
+                shutil.rmtree(preprocess_folder, ignore_errors=True)
+                shutil.rmtree(vectorize_folder, ignore_errors=True)
+                shutil.rmtree(predict_folder, ignore_errors=True)
+
+        # ---------------------
+        # PREPROCESSING
+        # ---------------------
+        logging.info("Running preprocessing for experiment %i - %s - %s", index, preprocess_hash, json.dumps(preprocess_params))
+
+        # make the output path
+        if not os.path.isdir(preprocess_folder):
+            os.makedirs(preprocess_folder)
+
+        # add to the list of hashes
+        if preprocess_hash in preprocess_params.keys():
+            preprocess_hashes[preprocess_hash] = preprocess_params
+
+        # run the preprocessing!
+        preprocess(preprocess_folder, **preprocess_params)
+
+        # ---------------------
+        # VECTORIZING
+        # ---------------------
+        logging.info("Running vectorization for experiment %i - %s - %s", index, vectorize_hash, json.dumps(vectorize_params))
+        
+        # make the output path
+        if not os.path.isdir(vectorize_folder):
+            os.makedirs(vectorize_folder)
+            
+        # add to the list of hashes
+        if vectorize_hash in vectorize_params.keys():
+            vectorize_hashes[vectorize_hash] = vectorize_params
+
+        # run the vectorizing!
+        vectorize(preprocess_folder, vectorize_folder, **vectorize_params)
+
+        # ---------------------
+        # PREDICTING
+        # ---------------------
+        logging.info("Running prediction for experiment %i - %s - %s", index, vectorize_hash, json.dumps(predict_params))
+        
+        # make the output path
+        if not os.path.isdir(predict_folder):
+            os.makedirs(predict_folder)
+            
+        # add to the list of hashes
         if predict_hash in predict_params.keys():
             predict_hashes[predict_hash] = predict_params
-        logging.info("Running prediction for experiment %i - %s - %s", index, vectorize_hash, json.dumps(predict_params))
+
+        # run the prediction!
         result = predict(**predict_params)
+
+        # gather result information
+        result.update(preprocess_params)
+        result.update(vectorize_params)
+        result.update(predict_params)
+        result.update({"preprocess":preprocess_hash, "vectorize":vectorize_hash, "predict":predict_hash})
+        result.update({"index":index})
 
         results_list.append(result)
 
         results_df = pd.DataFrame(results_list)
-        results_df.to_csv(experiment_path + "/results.csv")
+        results_df.to_csv(results_path)
+        results_df.to_csv(experiment_path + "/results_" + datetime.datetime.now().strftime("%Y-%m-%d"))
 
 
 def hash_params(params):
@@ -69,14 +148,43 @@ def hash_params(params):
     
 
 
-def preprocess(preprocess_hash, experiment_path, data_folder, count=-1, keywords=[], ignore_source=[], content_column="content", source_content="publication", **kwargs):
-    docify.docify(data_folder, experiment_path + preprocess_hash + "_documents.json")
+# data_folder
+# document_count (-1)
+# content_column ("content")
+# source_column ("publication")
+# keywords ([])
+# ignore_sources ([])
+def preprocess(preprocess_folder, **kwargs):
+    documents_file = preprocess_folder + "/documents.json"
+    
+    docify.docify(input_folder=kwargs.get("data_folder"), output_path=documents_file, count=kwargs.get("document_count", -1), content_column=kwargs.get("content_column", "content"), source_column=kwargs.get("source_column", "publication"), keywords=kwargs.get("keywords", []), ignore_source=kwargs.get("ignore_sources", []))
+    
+    tokenization.tokenize(input_file=documents_file, output_path=(preprocess_folder + "/tokens"))
 
 
-# methods: tfidf, as_vec
-def vectorize(method="tfidf", **kwargs):
-    pass
+# support
+# target_aspect_count (-1)
+# ner (False)
+# minimum_flr (10.0)
+# sentiment_distance_dist_sd (1)
+def vectorize(preprocess_folder, vectorize_folder, **kwargs):
+    documents_file = preprocess_folder + "/documents.json"
+    tokens_folder = preprocess_folder + "/tokens"
 
+    tfidf_path = vectorize_folder + "/tfidf.json"
+
+    aspect_data_path = vectorize_folder + "/aspects"
+
+    as_vec_path = vectorize_folder + "/as_vec"
+    
+    aspect_detection.bootstrap.detection.detect(input_path=tokens_folder, output_path=aspect_data_path, support=kwargs.get("support"), target_count=kwargs.get("target_aspect_count", -1), thread_count=THREAD_COUNT, named_entity_recog=kwargs.get("ner", False))
+
+    tfidfify.tfidf(input_path=documents_file, output_path=tfidf_path)
+
+    sentiment_analysis.as_vec.create_as_vectors(input_path=aspect_data_path, tokens_path=tokens_folder, output_path=as_vec_path, minimum_flr=kwargs.get("minimum_flr", 10.0), sd=kwargs.get("sentiment_distance_dist_sd", 1))
+
+    combine.combine(input_file_1=tfidf_path, input_file_2=(as_vec_path + "/doc_as_vectors.json"), output_file=(vectorize_folder + "/tfidf_as_vec_combined.json"))
+    
 
 def predict():
     pass
